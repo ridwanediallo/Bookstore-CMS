2c2429f0d6be159192a155b52d7277e9d1f165e0
ridwan <ridwanediallo99@gmail.com>
2022-05-08 00:58:07 +0000

    Remove initialState content

--- src/store/bookSlice.js
+++ src/store/bookSlice.js
@@ -2,11 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 export const bookSlice = createSlice({
   name: 'books',
-  initialState: [
-    { id: '1', title: 'Go pro', author: 'Ridwan' },
-    { id: '2', title: 'Master React', author: 'Ridwan' },
-    { id: '3', title: 'Learn Redux', author: 'Ridwan' },
-  ],
+  initialState: [],
   reducers: {
     bookAdded(state, action) {
       state.push(action.payload);
