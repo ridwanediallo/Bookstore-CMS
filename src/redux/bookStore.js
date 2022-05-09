df3ff499940fb2665a9c2266fb1ecccfbf498319
ridwan <ridwanediallo99@gmail.com>
2022-05-09 08:18:54 +0000

    Add CategoriesSlice

--- src/redux/bookStore.js
+++ src/redux/bookStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bookReducer from './books/bookSlice'
+import categoriesReducer from "./categories/categoriesSlice";
 
 export default configureStore({
-    reducer: {
-        books: bookReducer,
-    }
-})
+  reducer: {
+    books: bookReducer,
+    categories: categoriesReducer,
+  },
+});
