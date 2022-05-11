04b2aa2a04d13ca0ea7d454aa70a38af5172fdb4
ridwan <ridwanediallo99@gmail.com>
2022-05-11 14:40:39 +0000

    Move fetchBookData to Api.js

--- src/redux/Api.js
+++ src/redux/Api.js
@@ -1,3 +1,28 @@
+
+export const fetchBookData = (books) => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'https://react-http-project-35727-default-rtdb.firebaseio.com/books.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Could not fetch Book data');
+      }
+
+      const data = await response.json();
+
+      return data;
+    };
+
+    try {
+      await fetchData();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+};
+
 export const sendBookData = (books) => {
   const id = '1ixeASo4AU3X3cZnoiCd';
   return async (dispatch) => {
