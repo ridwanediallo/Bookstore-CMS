19a19181eb55453b0f4c405e18fe0dc571463b4e
ridwan <ridwanediallo99@gmail.com>
2022-05-13 20:13:41 +0000

    Update bookList component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,17 +1,13 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {
-  getBooksData,
-  removeBook,
-} from '../redux/books/bookSlice';
+import { getBooksData, removeBook } from '../redux/books/bookSlice';
 import './BookList.css';
 
 const BookList = () => {
   const dispatch = useDispatch();
   const books = useSelector((state) => state.books);
-  console.log(books); // the=is shows the data in the console
-  const booksData = Object.keys(books);
-
+  console.log('I am here', books); // the=is shows the data in the console
+  const booksKey = Object.keys(books);
 
   useEffect(() => {
     dispatch(getBooksData());
@@ -20,22 +16,29 @@ const BookList = () => {
   return (
     <div className="booklists">
       <ul className="list-group">
-        {booksData.map((book) => (
-          <li className="list-item" key={book.id}>
-            <p>{book.title}</p>
-            <p>{book.author}</p>
-            <div className="btns">
-              <button type="button">Comment</button>|
-              <button
-                type="button"
-                onClick={() => dispatch(removeBook(book.id))}
-              >
-                Remove
-              </button>
-              |<button type="button">Edit</button>
-            </div>
-          </li>
-        ))}
+        {booksKey.map((key) => {
+          const book = books[key];
+
+          const { category, author, title } = book[0];
+
+          return (
+            <li className="list-item" key={key}>
+              <p>{title}</p>
+              <p>{author}</p>
+              <p>{category}</p>
+              <div className="btns">
+                <button type="button">Comment</button>|
+                <button
+                  type="button"
+                  onClick={() => dispatch(removeBook(book.id))}
+                >
+                  Remove
+                </button>
+                |<button type="button">Edit</button>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
