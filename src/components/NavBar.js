13500a86faca33e546e772901f59e018c797e288
ridwan <ridwanediallo99@gmail.com>
2022-05-14 21:01:44 +0000

    Add Goperson Icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,38 @@
 
 import { Nav, Navbar, Container} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { IconContext } from 'react-icons';
+import {GoPerson} from 'react-icons/go'
 import './Navbar.css';
 
 const NavBar = () => (
-  <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+  <Navbar collapseOnSelect className="navig" expand="lg" bg="" variant="dark">
     <div className="container navB">
-      <Navbar.Brand href="#home" className="logo">
+      <Navbar.Brand href="#home" className=" text-primary logo">
         BookStore CMS
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
           <Nav.Link href="#book">
-            <Link to="/" className="link">
+            <Link to="/" className="text-black link">
               Books
             </Link>
           </Nav.Link>
           <Nav.Link href="#categories">
-            <Link to="/" className="link">
+            <Link to="/" className="text-secondary link">
               Categories
             </Link>
           </Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link href="#icon">Icon</Nav.Link>
+          <Nav.Link href="#icon">
+            <IconContext.Provider value={{ color: '#0290ff', size: '1em' }}>
+              <div className="userIcon">
+                <GoPerson />
+              </div>
+            </IconContext.Provider>
+          </Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </div>
